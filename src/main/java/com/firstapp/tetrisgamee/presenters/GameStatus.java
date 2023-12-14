d2acddd8589a39630ef357e56b4f6a97e05f0d2f
Gorur56 <sevim.gorur01@gmail.com>
2023-12-14 18:38:50 +0300

    Java Android Tetris Game Project

---
+++ src/main/java/com/firstapp/tetrisgamee/presenters/GameStatus.java
@@ -0,0 +1,10 @@
+package com.firstapp.tetrisgamee.presenters;
+
+public enum GameStatus {
+    START("START"),
+    PLAYING(PLAYING),
+    OVER("GAME OVER"),
+    PAUSED("GAME PAUSED");
+
+    private final String mValue;
+}
